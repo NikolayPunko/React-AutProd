9cd5341aa83778faaffdd64d634f7709c5ff66be
NikolayPunko <punkon2001@gmail.com>
2025-07-11 16:59:20 +0300

    Доработка планировщика

--- src/services/SchedulerService.js
+++ src/services/SchedulerService.js
@@ -25,34 +25,71 @@ const exampleTask = {
     bgColor: "#108EE9",
 };
 
-export default class SchedulerService {
+export default class SchedulerService {  //Осталось распарсить когда мойка
 
     static async parseParty(json) {
         const obj = JSON.parse(json);
-        for (let i = 0; i < obj.Projects.length; i++) {
+        console.log(obj)
+        for (let i = 0; i < obj.jobs.length; i++) {
             party[i] =  Object.assign({}, exampleResourse);
-            party[i].id = obj.Projects[i];
-            party[i].name = obj.Projects[i];
+            party[i].id = obj.jobs[i].id;
+            party[i].name = obj.jobs[i].name;
         }
-        // console.log(party)
         return party;
     }
 
+    // static async parseParty2(json) {
+    //     const obj = JSON.parse(json);
+    //     for (let i = 0; i < obj.Projects.length; i++) {
+    //         party[i] =  Object.assign({}, exampleResourse);
+    //         party[i].id = obj.Projects[i];
+    //         party[i].name = obj.Projects[i];
+    //     }
+    //     // console.log(party)
+    //     return party;
+    // }
+
     static async parseHardware(json) {
         const obj = JSON.parse(json);
-        for (let i = 0; i < obj.Resources.length; i++) {
+        for (let i = 0; i < obj.lines.length; i++) {
             hardware[i] =  Object.assign({}, exampleResourse);
-            hardware[i].id = obj.Resources[i];
-            hardware[i].name = obj.Resources[i];
+            hardware[i].id = obj.lines[i].id;
+            hardware[i].name = obj.lines[i].name;
         }
-        // console.log(hardware)
         return hardware;
     }
 
+    // static async parseHardware2(json) {
+    //     const obj = JSON.parse(json);
+    //     for (let i = 0; i < obj.Resources.length; i++) {
+    //         hardware[i] =  Object.assign({}, exampleResourse);
+    //         hardware[i].id = obj.Resources[i];
+    //         hardware[i].name = obj.Resources[i];
+    //     }
+    //     // console.log(hardware)
+    //     return hardware;
+    // }
+
+
     static async parsePlanByParty(json) {
         const obj = JSON.parse(json);
+        for (let i = 0; i < obj.jobs.length; i++) {
+            planByParty[i] = Object.assign({}, exampleTask);
+            planByParty[i].id = i;
+            planByParty[i].start = obj.jobs[i].startProductionDateTime.replace("T"," ");
+            planByParty[i].end = obj.jobs[i].endDateTime.replace("T"," ");
+            planByParty[i].title = obj.jobs[i].name;
+            planByParty[i].resourceId = obj.jobs[i].id;
+        }
+        // console.log(planByParty)
+        return planByParty;
+    }
+
+
+    static async parsePlanByParty2(json) {
+        const obj = JSON.parse(json);
         for (let i = 0; i < obj.AllocationList.length; i++) {
-            planByParty[i] = Object.assign({}, exampleTask);;
+            planByParty[i] = Object.assign({}, exampleTask);
             planByParty[i].id = i;
             planByParty[i].start = obj.AllocationList[i].StartDate.replace(""," ");
             planByParty[i].end = obj.AllocationList[i].EndDate.replace(""," ");
@@ -63,15 +100,29 @@ export default class SchedulerService {
         return planByParty;
     }
 
-    static async parsePlanByHardware(json) {
+    static async parsePlanByHardware2(json) {
         const obj = JSON.parse(json);
         for (let i = 0; i < obj.AllocationList.length; i++) {
-            planByHardware[i] = Object.assign({}, exampleTask);;
+            planByHardware[i] = Object.assign({}, exampleTask);
             planByHardware[i].id = i;
             planByHardware[i].start = obj.AllocationList[i].StartDate.replace(""," ");
             planByHardware[i].end = obj.AllocationList[i].EndDate.replace(""," ");
             planByHardware[i].title = obj.AllocationList[i].JID;
             planByHardware[i].resourceId = obj.AllocationList[i].ResourceRequirementList.length===0? "": obj.AllocationList[i].ResourceRequirementList[0];
+        }
+        // console.log(planByHardware)
+        return planByHardware;
+    }
+
+    static async parsePlanByHardware(json) {
+        const obj = JSON.parse(json);
+        for (let i = 0; i < obj.jobs.length; i++) {
+            planByHardware[i] = Object.assign({}, exampleTask);
+            planByHardware[i].id = i;
+            planByHardware[i].start = obj.jobs[i].startProductionDateTime.replace("T"," ");
+            planByHardware[i].end = obj.jobs[i].endDateTime.replace("T"," ");
+            planByHardware[i].title = obj.jobs[i].name;
+            planByHardware[i].resourceId = obj.jobs[i].line.id;
         }
         // console.log(planByHardware)
         return planByHardware;
