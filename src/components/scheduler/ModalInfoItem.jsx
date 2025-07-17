c0457047d5d57b6c87b206dfe0a9f0a8973737ce
NikolayPunko <punkon2001@gmail.com>
2025-07-17 16:23:05 +0300

    Разработана тестовая версия нового планировщика

--- src/components/scheduler/ModalInfoItem.jsx
+++ src/components/scheduler/ModalInfoItem.jsx
@@ -2,43 +2,60 @@ import React from 'react'
 
 
 export function ModalInfoItem({info, onClose}) {
+
+    const styleLable = "py-1 font-medium w-1/5 ";
+    const styleInfo = "py-1 font-medium w-4/5 ";
     return (
         <>
             <div
                 className="fixed bg-black/50 top-0 z-100 right-0 left-0 bottom-0" style={{zIndex: 99}}
                 onClick={onClose}
             />
-            <div class="fixed inset-0 flex items-center justify-center p-4 z-100 pointer-events-none"
+            <div className="fixed inset-0 flex  items-center justify-center p-4 z-100 pointer-events-none"
                  style={{zIndex: 100}}>
-                <div className="w-full max-w-[500px] bg-white rounded-lg p-5 px-8 pointer-events-auto">
+                <div className="w-auto min-w-[500px] bg-white rounded-lg p-5 px-8 pointer-events-auto">
                     <h1 className="text-xl font-medium text-start mb-2">{info.name}</h1>
                     <hr/>
+
+                    {info.fullName &&
+                        <div className="flex flex-row">
+                            <span className={styleLable}>Наименование:</span>
+                            <span className={styleInfo}>{info.fullName || ""}</span>
+                        </div>}
+
+                    {info.np &&
+                        <div className="flex flex-row">
+                            <span className={styleLable}>№ партии:</span>
+                            <span className={styleInfo}>{info.np || ""}</span>
+                        </div>}
+
+                    {info.type &&
+                        <div className="flex flex-row">
+                            <span className={styleLable}>Тип:</span>
+                            <span className={styleInfo}>{info.type || ""}</span>
+                        </div>}
+
+                    {info.quantity &&
+                        <div className="flex flex-row">
+                            <span className={styleLable}>Количество:</span>
+                            <span className={styleInfo}>{info.quantity || ""}</span>
+                        </div>}
+
                     <div className="flex flex-row">
-
-                        <div className="flex flex-col w-1/3">
-                            <span className="my-1 font-medium">Линия:</span>
-                            <span className="my-1 font-medium">Начало:</span>
-                            <span className="my-1 font-medium">Конец:</span>
-                            {info.quantity &&
-                                <span className="my-1 font-medium">Количество:</span>
-                            }
-
-                        </div>
-                        <div className="flex flex-col w-2/3">
-                            <span className="my-1">{info.line || ""}</span>
-                            <span className="my-1">{info.start || ""}</span>
-                            <span className="my-1">{info.end || ""}</span>
-                            <span className="my-1">{info.quantity || ""}</span>
-                        </div>
-
-
+                        <span className={styleLable}>Линия:</span>
+                        <span className={styleInfo}>{info.line || ""}</span>
                     </div>
-                    {/*<div className="flex flex-row justify-end">*/}
-                    {/*    <button onClick={onClose}*/}
-                    {/*            className="w-14 px-2 h-7 self-end  my-2 rounded text-sm font-medium shadow-sm border  bg-blue-800 hover:bg-blue-700 text-white">*/}
-                    {/*        ОК*/}
-                    {/*    </button>*/}
-                    {/*</div>*/}
+
+                    <div className="flex flex-row">
+                        <span className={styleLable}>Начало:</span>
+                        <span className={styleInfo}>{info.start || ""}</span>
+                    </div>
+
+                    <div className="flex flex-row">
+                        <span className={styleLable}>Конец:</span>
+                        <span className={styleInfo}>{info.end || ""}</span>
+                    </div>
+
 
                 </div>
             </div>
